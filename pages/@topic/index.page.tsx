80710fdf39141a8638d34c18e17401dd1159e4bb
cheetosysst <thect@protonmail.com>
2023-12-03 15:39:39 +0800

    feat: overall topic page update paginate description

--- pages/@topic/index.page.tsx
+++ pages/@topic/index.page.tsx
@@ -4,23 +4,34 @@ import type { PageContextBuiltInServer } from "vike/types";
 import { Layout } from "../layout";
 import { db } from "#/database/client";
 import { article, topic } from "#/database/schema";
+import { desc, eq, sql } from "drizzle-orm";
 
 export { Page };
 export const PrefetchSetting = { mode: "hover" };
 
-function Page({ name, page, articles, topics }: Props) {
+function Page({
+	id,
+	name,
+	description,
+	page,
+	articles,
+	topics,
+	pageCount,
+}: Props) {
 	return (
 		<Layout topics={topics}>
-			<h1 className="text-3xl">
-				{name}
-
+			<div className="flex justify-between items-center">
+				<h1 className="flex flex-col text-3xl">
+					{name}
+					<span className="text-lg text-black/80">{description}</span>
+				</h1>
 				<a
-					href={`/${name}/new`}
-					className="float-right drop-shadow-sm border-[1px] text-lg p-2 rounded-lg"
+					href={`/${id}/new`}
+					className="float-right drop-shadow-sm border-[1px] text-lg p-2 rounded-lg capitalize"
 				>
 					new
 				</a>
-			</h1>
+			</div>
 			<section className="flex flex-col gap-4">
 				{articles.map((item) => (
 					<Card
@@ -32,35 +43,87 @@ function Page({ name, page, articles, topics }: Props) {
 					/>
 				))}
 			</section>
-			<div>page: {page}</div>
+
+			<Pagination totalPages={pageCount} current={page} />
 		</Layout>
 	);
 }
+const Pagination = ({
+	totalPages,
+	current,
+}: {
+	totalPages: number;
+	current: number;
+}) => {
+	const renderPageNumbers = () => {
+		const pageNumbers = [];
+
+		// Calculate the range of page numbers to display
+		const startPage = Math.max(1, current - 4);
+		const endPage = Math.min(totalPages, current + 5);
+
+		for (let i = startPage; i <= endPage; i++) {
+			pageNumbers.push(
+				<li key={`paginate${i}`}>
+					<a
+						className={`p-2 rounded-md border-[1px] drop-shadow-sm hover:bg-sky-50 transition-colors ${
+							i === current ? "bg-neutral-300/60" : "bg-neutral-100/60"
+						}`}
+						href={`?page=${i}`}
+					>
+						{i}
+					</a>
+				</li>,
+			);
+		}
+		return pageNumbers;
+	};
+
+	return (
+		<ul className="flex gap-2 w-full justify-center">{renderPageNumbers()}</ul>
+	);
+};
 
 type Props = Topic & {
 	page: number;
 	articles: Array<Omit<Article, "content" | "topic" | "created">>;
 	topics: Array<Topic>;
+	pageCount: number;
 };
 
 export async function onBeforeRender(pageContext: PageContextBuiltInServer) {
+	const topicId = pageContext.urlOriginal.split("/")[1];
 	const data = await db.transaction(async (tx) => {
 		const articles = await tx
 			.select()
 			.from(article)
-			.orderBy(article.id)
+			.where(eq(article.topic, topicId))
+			.orderBy(desc(article.id))
 			.limit(10)
 			.offset(0);
 		const topics = await tx.select().from(topic).limit(10);
-		return { articles, topics };
+		const topicMeta = (
+			await tx.select().from(topic).where(eq(topic.id, topicId))
+		)[0];
+		const articleCount = (
+			await tx
+				.select({ count: sql<number>`cast(count(${article.id}) as int)` })
+				.from(article)
+				.where(eq(article.topic, topicId))
+		)[0].count;
+		return {
+			articles,
+			topics,
+			topicMeta,
+			pageCount: Math.ceil(articleCount / 10),
+		};
 	});
 	const props: Props = {
-		id: pageContext.routeParams.topic,
-		name: pageContext.routeParams.topic,
-		description: "",
-		page: +pageContext.urlParsed.search.page,
+		...data.topicMeta,
+		page: +pageContext.urlParsed.search.page || 1,
 		topics: data.topics,
 		articles: data.articles,
+		pageCount: data.pageCount,
 	};
 	return {
 		pageContext: {
